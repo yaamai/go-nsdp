ae81f65aab971899a062e13831ce825b7ef9d6ce
yaamai <ya_amai@yahoo.co.jp>
2020-11-29 18:19:52 +0000

    dump

--- main.go
+++ main.go
@@ -2,11 +2,40 @@ package main
 
 import (
 	"bytes"
+	"encoding/json"
+	"fmt"
 	"gs308e/nsdp"
+	"io/ioutil"
 	"log"
+	"os"
+	"os/signal"
+	"syscall"
 )
 
 func main() {
+	data := map[string]string{}
+
+	dataBytes, err := ioutil.ReadFile("dump.json")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	json.Unmarshal(dataBytes, &data)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+
+	go func() {
+		<-sigs
+
+		jsonBytes, err := json.MarshalIndent(data, "", "\t")
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		ioutil.WriteFile("dump.json", jsonBytes, 0644)
+		os.Exit(0)
+	}()
+
 	c, err := NewClient()
 	if err != nil {
 		log.Fatalln(err)
@@ -14,11 +43,15 @@ func main() {
 
 	var cur, prev *nsdp.Msg
 	for idx := 1; idx < 0xffff; idx++ {
-		if idx >= 0x1000 && idx <= 0x11ff {
+		if _, ok := data[fmt.Sprintf("%x", idx)]; ok {
+			continue
+		}
+		if idx >= 0x1000 && idx <= 0x13ff {
 			continue
 		}
 		cur = c.Read(&nsdp.TLVBase{Tag: uint16(idx), Length: 0})
 		log.Printf("%x, %v", idx, cur)
+		data[fmt.Sprintf("%x", idx)] = fmt.Sprintf("%v", cur)
 
 		if cur != nil && prev != nil {
 			prev.Seq = cur.Seq
